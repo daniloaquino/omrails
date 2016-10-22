c87c149fe3cdf79808901a65fc6c7abb9dd0c93d
Danilo Aquino <daquino@utexas.edu>
2016-10-22 19:31:48 +0400

    Updated CSS for Sign Up and Sign In pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  as :user do
+  	get "signin" => 'devise/sessions#new'
+  	delete "signout" => 'devise/sessions#destroy'
+  	get "signup" => 'devise/registrations#new'
+  end
+
   root 'pages#home'
   get 'about' => 'pages#about'
-  get 'contactus' => 'pages#contactus'
+  get 'contact_us' => 'pages#contact_us'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
