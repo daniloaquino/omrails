ae09aab744061efb679604e6234382f6a438eb49
Danilo Aquino <daquino@utexas.edu>
2016-11-05 15:04:56 +0400

    Add user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
+  get 'users/:username', to: 'users#show', as: 'user'
+
   resources :items
   resources :links
   resources :tweets
   ActiveAdmin.routes(self)
   devise_for :users
   as :user do
-  	get "signin" => 'devise/sessions#new'
-  	delete "signout" => 'devise/sessions#destroy'
-  	get "signup" => 'devise/registrations#new'
+  	get "signin", to: 'devise/sessions#new'
+  	delete "signout", to: 'devise/sessions#destroy'
+  	get "signup", to: 'devise/registrations#new'
   end
 
   root 'pages#home'
-  get 'about' => 'pages#about'
-  get 'contact_us' => 'pages#contact_us'
+  get 'about', to: 'pages#about'
+  get 'contact_us', to: 'pages#contact_us'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
